ba2291ba63e24eeeb8f986a983792f8c1360ed0f
Dilan Ozkaynak <dio4@pitt.edu>
2022-03-19 15:16:33 -0400

    IWorkspace ui

--- src/misc.ts
+++
@@ -1,7 +0,0 @@
-export interface IWorkspace {
-    titles_arr: string[];
-    root_db: string;
-    workspaces_arr: string[];
-}
-
-export type PricingTier = "Plus" | "Growth" | "Plus annual" | "Growth annual" | "Bootstrapper" | "Enterprise" | "Free";
